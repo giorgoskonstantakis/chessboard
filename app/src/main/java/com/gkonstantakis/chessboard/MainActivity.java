ff628aef142ea3aa2bd5511c5af75ced471d83ca
Giorgos Konstantakis <g.konstantakis@novelty-media.com>
2021-08-31 12:20:21 +0300

    Setup app's fragments and navigation

--- app/src/main/java/com/gkonstantakis/chessboard/MainActivity.java
+++ app/src/main/java/com/gkonstantakis/chessboard/MainActivity.java
@@ -1,6 +1,10 @@
 package com.gkonstantakis.chessboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavGraph;
+import androidx.navigation.NavInflater;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupActivityFragment();
+    }
+
+    private void setupActivityFragment() {
+        FragmentManager manager = getSupportFragmentManager();
+        NavHostFragment navHostFragment = (NavHostFragment) manager.findFragmentById(R.id.nav_host_fragment);
+        NavInflater navInflater = navHostFragment.getNavController().getNavInflater();
+        NavGraph navGraph = navInflater.inflate(R.navigation.nav_graph);
+        navGraph.setStartDestination(R.id.setupFragment);
+        navHostFragment.getNavController().setGraph(navGraph);
     }
 }
